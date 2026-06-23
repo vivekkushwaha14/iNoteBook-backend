918ff18e9e2bfe99f8e59efb8b388e395a0c251b
vivekkushwaha14 <vivekkushwaha8654v@gmail.com>
2026-06-23 11:36:20 +0530

    some error

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,7 +1,6 @@
 var jwt = require('jsonwebtoken');
 
 const fetchuser = (req, res, next) => { 
-// Get the token  from the jwt  token and add id to request object
 const token = req.header("auth-token")
 if (!token) {
     res.status(401).send({ error: "please authenticate using a valid token" })
